6bb963ae7009ac4342edaec6662bb3119cadbb93
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2022-07-18 17:59:35 +0200

    Add test and try to get good values

--- geneticAlgoritm.py
+++ geneticAlgoritm.py
@@ -9,25 +9,30 @@ class GeneticAlgorithm:
     def stepGeneration(self):
         fitnesses = [self.fitnessFunc(i) for i in self.population]
 
-        print("Fitnesses: {}".format(fitnesses))
+        #print("Fitnesses: {}".format(fitnesses))
 
         maxFitness = max(fitnesses)
+
+        best = self.population[fitnesses.index(maxFitness)]
+
         if self.fitnessTarget and maxFitness >= self.fitnessTarget:
             print("Reached target fitness: {}".format(maxFitness))
             return
 
         # Repopulate with roulette wheel selection
-        self.population = [g.clone() for g in random.choices(
-            self.population,
-            weights=fitnesses,
-            k=len(self.population)
-        )]
+        if maxFitness != 0: # Cannot sample with invalid fitness
+            self.population = [g.clone() for g in random.choices(
+                self.population,
+                weights=fitnesses,
+                k=len(self.population)
+            )]
         for g in self.population:
             g.mutate()
 
-        return maxFitness
+        return maxFitness, best
 
     def run(self, nGenerations):
         for gen in range(nGenerations):
-            maxFitness = self.stepGeneration()
-            print("Generation {}, max fitness {}".format(gen, maxFitness))
+            maxFitness, best = self.stepGeneration()
+            print("Generation {}\n  Max fitness: {}\n  Best genome: {}".format(gen, maxFitness, best))
+        return maxFitness, best
--- genome.py
+++ genome.py
@@ -1,6 +1,7 @@
 
-import numpy as np
+import math
 import random
+import networkx as nx
 
 class Genome:
     def __init__(self):
@@ -46,31 +47,60 @@ class ArrayGenome(Genome):
                 if self.minVal is not None:
                     self.values[i] = max(0, self.values[i])
 
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+
+# Activation functions:
+
+fActs = ['lin', 'sigm', 'log', 'tanh', 'relu']
+def activate(x, fAct='lin'):
+    if fAct == 'lin': # Linear
+        return x
+    elif fAct == 'sigm': # Sigmoid
+        return 1/(1 + math.exp(-x))
+    elif fAct == 'log':
+        return math.log(abs(x)+1)
+    elif fAct == 'sin':
+        return math.sin(x)
+    elif fAct == 'tanh':
+        return math.tanh(x)
+    elif fAct == 'relu':
+        return x if x>0 else 0
+
+    raise ValueError("Unknown activation function: '{}'".format(fAct))
 
 class NeatNode:
-    def __init__(self, inputs=[], fAct=sigmoid):
+    def __init__(self, inputs=[], fAct='sigm'):
         self.inputs = inputs
         self.fAct = fAct
+        self.val = 0
 
     def getValue(self, passedNodes=[]):
         # Avoid recurrance by keeping track of passed nodes
-        return self.fAct(sum(input['weight'] * input['node'].getValue(passedNodes=[self, *passedNodes]) for input in self.inputs if not input['node'] in passedNodes))
+        return activate(
+            sum(input['weight'] * input['node'].getValue(passedNodes=[self, *passedNodes]) for input in self.inputs if not input['node'] in passedNodes),
+            self.fAct
+        )
+
+    def updateValFromInput(self):
+        self.val = activate(
+            sum(input['weight'] * input['node'].val for input in self.inputs),
+            self.fAct
+        )
 
 class NeatOutputNode(NeatNode):
-    def __init__(self, inputs=[], fAct=sigmoid):
+    def __init__(self, inputs=[], fAct='lin'):
         super().__init__(inputs, fAct)
         self.prefix = 'o'
 
 
 class NeatHiddenNode(NeatNode):
-    def __init__(self, inputs=[], fAct=sigmoid):
+    def __init__(self, inputs=[], fAct=None):
+        if fAct == None:
+            fAct = random.choice(fActs)
         super().__init__(inputs, fAct)
         self.prefix = 'h'
 
 class NeatInputNode(NeatNode):
-    def __init__(self, fAct=sigmoid):
+    def __init__(self, fAct='lin'):
         self.fAct = fAct
         self.val = 0
         self.prefix = 'i'
@@ -79,11 +109,15 @@ class NeatInputNode(NeatNode):
         self.val = val
 
     def getValue(self, passedNodes=[]):
-        return self.fAct(self.val)
+        return activate(self.val, self.fAct)
 
-class BiasNode(NeatInputNode):
+    def updateValFromInput(self):
+        pass
+
+class NeatBiasNode(NeatInputNode):
     def __init__(self):
         self.prefix = 'b'
+        self.val = 1
         return
     def getValue(self, passedNodes=[]):
         return 1
@@ -91,58 +125,78 @@ class BiasNode(NeatInputNode):
 class NeatGenome(Genome):
     def __init__(self, nInputs, nOutputs, mutationWeights=[
         10, # Mutate connection weight,
-        3,  # Remove connection,
-        2,  # Add connection,
-        3   # Add node
-    ], mutationRate = 1):
+        4,  # Remove connection,
+        4,  # Add connection,
+        1   # Add node
+    ], mutationRate = 0.75):
         super().__init__()
         self.inputs = [NeatInputNode() for _ in range(nInputs)]
-        self.bias = BiasNode()
+        self.bias = NeatBiasNode()
         self.hidden = []
         self.outputs = [NeatOutputNode(
             # Make fully connected
-            inputs=[{'weight': 1, 'node': node} for node in [*self.inputs, self.bias]]
+            inputs=[{'weight': 1.0, 'node': node} for node in self.inputs] #[*self.inputs, self.bias]]
         ) for i in range(nOutputs)]
         self.mutationWeights = mutationWeights
         self.mutationRate = mutationRate
 
     def clone(self):
+
+        # Stringify connections
+        s = str(self)
+
         # Create new genome with correct number of nodes
         other = NeatGenome(
             len(self.inputs), len(self.outputs),
             self.mutationWeights, self.mutationRate
         )
-        other.hidden = [NeatHiddenNode(fAct=n.fAct) for n in self.hidden]
 
-        # Map old nodes to new ones
-        oldInNodes = [*self.hidden, *self.inputs, self.bias]
-        newInNodes = [*other.hidden, *other.inputs, other.bias]
-        assert len(oldInNodes) == len(newInNodes)
-        oldOutNodes = [*self.outputs, *self.hidden]
-        newOutNodes = [*other.outputs, *other.hidden]
-        assert len(oldOutNodes) == len(newOutNodes)
+        other.hidden = [NeatHiddenNode() for n in self.hidden]
 
-        # Clear new connectivity
-        for n in newOutNodes:
+        # Clear connectivity
+        for n in [*other.outputs, *other.hidden]:
             n.inputs = []
 
-        # Clone connectivity from old genome
-        for i, n in enumerate(oldOutNodes):
-            # Copy activation function
-            newOutNodes[i].fAct = n.fAct
-            # Create input connections with old
-            # connectivity and weights
-            newOutNodes[i].inputs = [{
-                'weight': oldInput['weight'],
-                'node': newInNodes[oldInNodes.index(oldInput['node'])]
-            } for oldInput in n.inputs]
-        for i, n in enumerate(self.inputs):
-            other.inputs[i].fAct = n.fAct
-            other.inputs[i].val  = n.val
+        nodeLabelMap = {other.labelNode(n): n  for n in [
+            *other.inputs, *other.outputs, other.bias, *other.hidden
+        ]}
 
-        other.age = self.age + 1
+        if s != "":
+            for connection in s.split(', '):
+                iNodeStr, weightStr, oNodeStr = connection.split('~')
+                oNodeStr, fAct = oNodeStr.split('_')
+                for nStr in [iNodeStr, oNodeStr]:
+                    if not nStr in nodeLabelMap:
+                        raise ValueError("Mystery node "+nStr)
+                iNode = nodeLabelMap.get(iNodeStr)
+                oNode = nodeLabelMap.get(oNodeStr)
+                oNode.fAct = fAct
+                weight = float(weightStr)
+                oNode.inputs.append({
+                    'weight': weight,
+                    'node': iNode
+                })
+
+        # assert str(other) == s, "Incorrect copy \n\t{}\n\t{}".format(s, str(other))
+        assert len(other.hidden) <= len(self.hidden), "{} != {}".format(len(other.hidden), len(self.hidden))
 
         return other
+
+    def getGraph(self):
+        G = nx.Graph()
+        for c in self.allConnections():
+            G.add_edge(
+                self.labelNode(c['input']['node']), 
+                self.labelNode(c['output']),
+                weight=c['input']['weight']
+            )
+        return G
+
+    def draw(self):
+        nx.draw_networkx(
+            self.getGraph(),
+            arrows=True
+        )
 
     def allConnections(self):
         return ({
@@ -161,29 +215,30 @@ class NeatGenome(Genome):
             return None
 
     def mutateAddConnection(self):
-        print("Adding connection")
+        #print("Adding connection")
         allInput = [*self.inputs, *self.hidden, self.bias]
         allOutput = [*self.hidden, *self.outputs]
 
         input = random.choice(allInput)
         output = random.choice(allOutput)
 
-        output.inputs.append({'weight': 0, 'node': input})
+        output.inputs.append({'weight': 0.0, 'node': input})
 
     def mutateRemoveConnection(self):
         c = self.getRandomConnection()
         if not c:
             return
 
-        print("Removing connection {}-{}-{}".format(
-            self.labelNode(c['input']['node']),
-            c['input']['weight'],
-            self.labelNode(c['output']))
-        )
+        #print("Removing connection {}-{}-{}".format(
+        #    self.labelNode(c['input']['node']),
+        #    c['input']['weight'],
+        #    self.labelNode(c['output']))
+        #)
 
         # Remove connection
         c['output'].inputs.remove(c['input'])
 
+        """
         # Remove hidden nodes they are no longer connected
         if not c['output'].inputs and c['output'] in self.hidden:
             print("Also removing node {}".format(
@@ -195,27 +250,28 @@ class NeatGenome(Genome):
             for d in self.allConnections():
                 if d['input']['node'] is c['output']:
                     d['output'].inputs.remove(d['input'])
+        """
 
     def mutateConnection(self):
-        print("Mutating connection strength")
+        #print("Mutating connection strength")
         c = self.getRandomConnection()
         if not c:
             return
         c['input']['weight'] += random.gauss(0, 0.1)
 
     def mutateAddNode(self):
-        print("Adding node")
+        #print("Adding node")
         # Add a node in place of an existing connection
         c = self.getRandomConnection()
         if not c:
             return
         newNode = NeatHiddenNode(
-            inputs = [c['input']] # Keep same weight
-            # Add random activation function here?
+            inputs = [c['input']], # Keep same weight
+            fAct = random.choice(fActs) # Random activation function
         )
         i = c['output'].inputs.index(c['input'])
         c['output'].inputs[i] = {
-            'weight': 1, # Use unit weight on second connection
+            'weight': 1.0, # Use unit weight on second connection
             'node': newNode
         }
         self.hidden.append(newNode)
@@ -237,16 +293,32 @@ class NeatGenome(Genome):
 
         return [output.getValue() for output in self.outputs]
 
+    def evaluate2(self, inputVals, nSteps=100):
+        for i, v in enumerate(inputVals):
+            self.inputs[i].setValue(v)
+        for i in range(nSteps):
+            for node in [*self.hidden, *self.outputs]:
+                node.updateValFromInput()
+
+        return [output.val for output in self.outputs]
+
     def labelNode(self, n):
         nodes = [*self.inputs, self.bias, *self.outputs, *self.hidden]
-        return n.prefix+chr(ord('@')+nodes.index(n)+1)
+        return (
+            n.prefix +
+            # chr(ord('@') + nodes.index(n)+1)
+            str(nodes.index(n))
+        )
 
     def __str__(self) -> str:
-        return ', '.join("{}~{}~{}".format(
+        return ', '.join("{}~{}~{}_{}".format(
             self.labelNode(c['input']['node']),
             c['input']['weight'],
-            self.labelNode(c['output'])
+            self.labelNode(c['output']),
+            c['output'].fAct
         ) for c in self.allConnections())
+
+
 
 def int2char(n):
     return chr(ord('@')+n)
